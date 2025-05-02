4b7048ebeca4160920db1dff5f54f0b8f8514b08
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-05-02 18:50:06 +0530

    db.js create functions

---
+++ js/db.js
@@ -0,0 +1,18 @@
+const db = {
+    customers: []
+};
+
+function saveCustomer(customer) {
+}
+
+function getCustomers() {
+}
+
+function removeCustomer(id) {
+}
+
+function updateCustomer(customer) {
+}
+
+function getCustomerById(id) {
+}
